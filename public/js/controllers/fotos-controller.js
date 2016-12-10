74dbcf06d1a7600aadfddf99d260832717566606
joaodematejr <joaodematejr@gmail.com>
2016-12-10 17:25:51 -0200

    Crud Inicial
    Crud Utilizando AngularJS e NodeJS - Inicio

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,21 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http){
+
+	$scope.fotos = [];
+	$scope.filtro = '';
+
+	$http.get('v1/fotos')
+	.success(function(fotos){
+		$scope.fotos = fotos;
+	})
+	.error(function(erro){
+		console.log(erro);
+	});
+/*
+	var promise = $http.get('v1/fotos');
+	promise.then(function(retorno){
+		$scope.fotos = retorno.data;
+	}).catch(function(erro){
+		console.log(erro);
+	});
+	*/
+});
