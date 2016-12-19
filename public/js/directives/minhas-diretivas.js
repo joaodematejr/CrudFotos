c7d9a3b6c10a15d40dcb111bd051b9c41c794969
joaodematejr <joaodematejr@gmail.com>
2016-12-19 19:48:50 -0200

    Atualização Implementando CheckBox
    Atualização Implementando CheckBox

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -19,4 +19,16 @@
 		ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';           
 		return ddo;
 
+	})
+	.directive('meuBotaoPerigo',function(){
+		var ddo = {}
+		ddo.restrict = "E";
+
+		ddo.scope = {
+			nome: '@',
+			acao: '&'	
+		};
+		ddo.template ='<button ng-click="acao(foto)" class="btn btn-danger btn-block">{{nome}}</button>';
+		return ddo;
+
 	});
